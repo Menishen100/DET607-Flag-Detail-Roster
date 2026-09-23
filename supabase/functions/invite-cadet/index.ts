3bc2055fbc1c5b51ff7e4bf40c508630ff99687b
Menishen100 <mafriyie@broncos.uncfsu.edu>
2026-09-22 21:09:21 -0400

    Permit invite function POST requests

--- supabase/functions/invite-cadet/index.ts
+++ supabase/functions/invite-cadet/index.ts
@@ -1,5 +1,5 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-const headers={"Access-Control-Allow-Origin":"https://det607flagdetail.com","Access-Control-Allow-Headers":"authorization, x-client-info, apikey, content-type","Content-Type":"application/json"};
+const headers={"Access-Control-Allow-Origin":"https://det607flagdetail.com","Access-Control-Allow-Methods":"POST, OPTIONS","Access-Control-Allow-Headers":"authorization, x-client-info, apikey, content-type","Content-Type":"application/json"};
 Deno.serve(async request=>{
  if(request.method==='OPTIONS')return new Response('ok',{headers});
  if(request.method!=='POST')return new Response(JSON.stringify({error:'Method not allowed'}),{status:405,headers});
