71889e6e87927496ac960478400002088b61c9c6
Menishen100 <mafriyie@broncos.uncfsu.edu>
2026-09-22 21:11:26 -0400

    Use supported invite function dependency

--- supabase/functions/invite-cadet/index.ts
+++ supabase/functions/invite-cadet/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "npm:@supabase/supabase-js@2";
 const headers={"Access-Control-Allow-Origin":"https://det607flagdetail.com","Access-Control-Allow-Methods":"POST, OPTIONS","Access-Control-Allow-Headers":"authorization, x-client-info, apikey, content-type","Content-Type":"application/json"};
 Deno.serve(async request=>{
  if(request.method==='OPTIONS')return new Response('ok',{headers});
